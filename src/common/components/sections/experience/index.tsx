b03cf52cbdc2624fa4138af7392c3320ba8e7576
AkhileshwarReddySongala <akhileshwar.songala@gmail.com>
2025-08-03 16:24:08 -0700

    first commit

--- src/common/components/sections/experience/index.tsx
+++ src/common/components/sections/experience/index.tsx
@@ -21,47 +21,49 @@ export default function Experience() {
     <section
       id="experience"
       ref={ref}
-      className="w-full scroll-mt-20 dark:bg-darkBg dark:text-white"
+      className="w-full flex flex-col items-center scroll-mt-20 dark:bg-darkBg dark:text-white"
     >
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline animate={false}>
-        {ihasMounted &&
-          experiencesData.map((item, index) => (
-            <React.Fragment key={index}>
-              <VerticalTimelineElement
-                contentStyle={{
-                  background:
-                    theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
-                  boxShadow: 'none',
-                  border: '1px solid rgba(0, 0, 0, 0.05)',
-                  textAlign: 'left',
-                  padding: '1.3rem 2rem',
-                }}
-                contentArrowStyle={{
-                  borderRight:
-                    theme === 'light'
-                      ? '0.4rem solid #9ca3af'
-                      : '0.4rem solid rgba(255, 255, 255, 0.5)',
-                  display: theme === 'dark' ? 'none' : 'block',
-                }}
-                date={item.date}
-                icon={item.icon}
-                iconStyle={{
-                  background:
-                    theme === 'light' ? '#fff' : 'rgba(255, 255, 255, 0.15)',
-                  fontSize: '1.5rem',
-                  display: theme === 'dark' ? 'none' : 'flex',
-                }}
-              >
-                <h3 className="font-semibold">{item.title}</h3>
-                <p className="!mt-0 font-normal">{item.location}</p>
-                <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                  {item.description}
-                </p>
-              </VerticalTimelineElement>
-            </React.Fragment>
-          ))}
-      </VerticalTimeline>
+      <div className="w-full max-w-4xl mx-auto">
+        <VerticalTimeline animate={false}>
+          {ihasMounted &&
+            experiencesData.map((item, index) => (
+              <React.Fragment key={index}>
+                <VerticalTimelineElement
+                  contentStyle={{
+                    background:
+                      theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
+                    boxShadow: 'none',
+                    border: '1px solid rgba(0, 0, 0, 0.05)',
+                    textAlign: 'left',
+                    padding: '1.3rem 2rem',
+                  }}
+                  contentArrowStyle={{
+                    borderRight:
+                      theme === 'light'
+                        ? '0.4rem solid #9ca3af'
+                        : '0.4rem solid rgba(255, 255, 255, 0.5)',
+                    display: theme === 'dark' ? 'none' : 'block',
+                  }}
+                  date={item.date}
+                  icon={item.icon}
+                  iconStyle={{
+                    background:
+                      theme === 'light' ? '#fff' : 'rgba(255, 255, 255, 0.15)',
+                    fontSize: '1.5rem',
+                    display: theme === 'dark' ? 'none' : 'flex',
+                  }}
+                >
+                  <h3 className="font-semibold">{item.title}</h3>
+                  <p className="!mt-0 font-normal">{item.location}</p>
+                  <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+                    {item.description}
+                  </p>
+                </VerticalTimelineElement>
+              </React.Fragment>
+            ))}
+        </VerticalTimeline>
+      </div>
       <div className="flex w-full justify-center dark:bg-darkBg">
         <SectionDivider />
       </div>
